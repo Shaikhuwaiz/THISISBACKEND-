9661a4b818ca0ff6e360d6e39665a51e57d23d94
Owaiz Shaikh <your_github_email@example.com>
2025-09-27 22:52:06 +0530

    fix: update server.js port binding and remove PORT from env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const itemRoutes = require("./routes/itemRoutes");
 const shipmentRoutes = require("./routes/shipmentRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 7001;   // Railway will inject its own PORT
+const PORT = process.env.PORT || 8000;   // Railway will inject its own PORT
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/shipmentsDB";
 
 // Middleware
@@ -31,6 +31,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
